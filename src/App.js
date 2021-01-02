51f622a463a9a036dee1d899c94d888880bf1772
hu.wenbo <hu.wenbo2mekitec.com>
2021-01-02 15:43:59 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
+const tec_list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org/',
+        author:'Jordan Walke',
+        num_comments:3,
+        points:4,
+        objectID:0,
+
+    },
+
+    {
+        title:'Redux',
+        url:'https://redux.js.org/',
+        author:'Dan Abramov,Andrew Clark',
+        num_comments:2,
+        points:5,
+        objectID:1,
+    },
+    
+];
+
+
+function getTitle(title)
+{
+    return title;
+}
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <h1>Hello {getTitle('React')}</h1>
+
+          <label htmlFor="search">Search: </label>
+          <input id="search" type="text"/>
+
+          <hr/>
+          {
+              tec_list.map(function(item){
+                  return <div>{item.title}</div>;}
+                          )
+          }
+
     </div>
   );
 }
